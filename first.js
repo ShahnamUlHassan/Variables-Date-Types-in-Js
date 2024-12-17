c86702796e981d71ccebe24182a7d7d49946c476
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2024-12-17 15:03:26 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,74 @@
+// console.log("Hello World");
+// console.log("Shahnam ul Hassan");
+// console.log("I Love Js");
+
+// name = "moon";
+// age=26;
+// price=99.49;
+// x= "null";
+// y="undefined";
+// isFollow="false";
+// isFollow="true";
+// console.log(isFollow);
+
+// variables
+
+
+// let name = "moon";
+// let age = 26;
+// let price = 1000;
+
+// var age =26;
+
+// var age = 30;
+
+// var age = 35;
+
+
+// const a=26;
+
+// console.log(a);
+
+// {let a = 5;
+//     console.log(a);
+// }
+// {
+//     let a =10;
+//     console.log(a);
+// }
+
+
+// Data Type 
+
+// Number,String,Boolean,Undefined,Null,BigInt,Symbols
+
+
+// let x = BigInt("123");
+
+// const student = {
+//     fullname :"moon",
+//     age : 26,
+//     CGPA : 8.2,
+//     isPass : true,
+// };
+// student["age"]= student["age"] +1;
+// student["name"] = "moon2";
+// console.log(student["name"]);
+
+
+// const product ={
+//     title : "Ball Pen",
+//     rating : 4,
+//     offer : 5,
+//     price : 270,
+// };
+// console.log(product);
+
+const profile ={
+    username : "shahnamulhassan",
+    isFollow:false,
+    followers:49,
+    folloing:3,
+};
+console.log(profile);
+console.log(typeof profile["followers"]);
